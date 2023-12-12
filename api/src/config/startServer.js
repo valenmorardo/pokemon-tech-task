42a5d98f9b295eceba9091eb6c6218aeae6e6843
Valentin <valentin.morardo@gmail.com>
2023-12-11 22:03:01 -0300

    startServer function added

---
+++ api/src/config/startServer.js
@@ -0,0 +1,18 @@
+import server from '@config/server.cfg';
+
+const startServer = () => {
+	return new Promise((resolve, reject) => {
+		try {
+			server.listen(server.get('port'), () => {
+				console.log(`Server running on ---> ${server.get('port')}`);
+				resolve();
+			});
+		} catch (error) {
+			console.log('EROR!!');
+			reject(error);
+		}
+		return true;
+	});
+};
+
+export default startServer;
