59221ec7d30ec4397434572e77c8de8bd582e568
Valentin <valentin.morardo@gmail.com>
2023-12-12 13:01:27 -0300

    paginado added to home

--- client/app/src/components/PokemonesContainer/PokemonesContainer.tsx
+++ client/app/src/components/PokemonesContainer/PokemonesContainer.tsx
@@ -7,6 +7,7 @@ import { getPokemonesAction } from "../../redux/actions";
 
 import Cards from "./Cards/Cards";
 import { IonLoading } from "@ionic/react";
+import Paginado from "./Paginado/Paginado";
 
 
 const PokemonesContainer = () => {
@@ -17,13 +18,25 @@ const PokemonesContainer = () => {
 
   useEffect(() => {
     dispatch(getPokemonesAction());
-  }, []);
+  }, [dispatch]);
+
+
+  const [pagina, setPagina] = useState(1);
+  const [porPagina, setPorPagina] = useState(6);
+  const maximo = pokemones?.length / porPagina;
+  const currentPokemones = pokemones?.slice(
+    (pagina - 1) * porPagina,
+    (pagina - 1) * porPagina + porPagina
+  );
+
 
   return (
     <>
       {pokemones ? (
         <div>
-          <Cards pokemones={pokemones} />
+          <Paginado pagina={pagina} setPagina={setPagina} maximo={maximo}/>
+          <Cards pokemones={currentPokemones} />
+          <Paginado pagina={pagina} setPagina={setPagina} maximo={maximo}/>
         </div>
       ) : <IonLoading  message="Loading data..." duration={3000} animated={true} spinner={'dots'} isOpen={true}/>}
     </>
