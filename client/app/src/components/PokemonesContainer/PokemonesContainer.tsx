da4404b78c582a2d38e6e41712254b7a32bb4005
Valentin <valentin.morardo@gmail.com>
2023-12-12 12:29:54 -0300

    loading added

--- client/app/src/components/PokemonesContainer/PokemonesContainer.tsx
+++ client/app/src/components/PokemonesContainer/PokemonesContainer.tsx
@@ -4,26 +4,29 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { getPokemonesAction } from "../../redux/actions";
 
+
 import Cards from "./Cards/Cards";
+import { IonLoading } from "@ionic/react";
 
 
 const PokemonesContainer = () => {
+  const dispatch = useDispatch();
 
+  const PokemonesRedux = useSelector((state: any) => state.pokemones);
+  const pokemones = PokemonesRedux.data?.pokemon_v2_pokemon;
 
-    const dispatch = useDispatch();
-
-    const PokemonesRedux = useSelector((state: any) => state.pokemones)
-    const pokemones = PokemonesRedux.data?.pokemon_v2_pokemon
-
-    useEffect(() => {
-        dispatch(getPokemonesAction())
-    }, [])
+  useEffect(() => {
+    dispatch(getPokemonesAction());
+  }, []);
 
   return (
-    <div>
-      <Cards pokemones = {pokemones}/>
-
-    </div>
+    <>
+      {pokemones ? (
+        <div>
+          <Cards pokemones={pokemones} />
+        </div>
+      ) : <IonLoading  message="Loading data..." duration={3000} animated={true} spinner={'dots'} isOpen={true}/>}
+    </>
   );
 };
 
