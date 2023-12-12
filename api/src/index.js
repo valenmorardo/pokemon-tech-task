6ae706da5f225141cb3dce730758a944b17cf570
Valentin <valentin.morardo@gmail.com>
2023-12-11 22:08:50 -0300

    run app function ready

--- api/src/index.js
+++ api/src/index.js
@@ -8,3 +8,23 @@
 //		─▄█████.▼.▼.▼.▼.▼.▼▼▼▼
 //		▄███████▄.▲.▲▲▲▲▲▲▲▲
 //		████████████████████▀▀
+
+
+import startServer from "./config/startServer.js";
+
+const runApp = async () => {
+	try {
+		await startServer();
+	} catch (err) {
+		console.log('ERROR !!!');
+		if (typeof err === 'string') {
+			throw new Error(err);
+		} else {
+			console.log(err);
+			throw new Error('An unknown error occurred. Check start server function.');
+		}
+	}
+
+	return true;
+};
+runApp();
