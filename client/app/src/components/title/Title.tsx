7080f244d6fe70a37f5278efd6540f3744e811ef
Valentin <valentin.morardo@gmail.com>
2023-12-12 12:17:34 -0300

    title refactor

--- client/app/src/components/title/Title.tsx
+++ client/app/src/components/title/Title.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { IonItem, IonLabel } from "@ionic/react";
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
 
 const Title: React.FC = () => {
   return (
-    <IonItem>
-      <IonLabel>
-        <h1>Pokedex</h1>
-      </IonLabel>
-    </IonItem>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Pokedex</IonTitle>
+      </IonToolbar>
+    </IonHeader>
   );
 };
 
