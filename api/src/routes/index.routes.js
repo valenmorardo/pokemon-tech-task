772cb86b64b23ab8ed39d33228ef994455196e44
Valentin <valentin.morardo@gmail.com>
2023-12-11 22:49:26 -0300

    C:/Program Files/Git/pokemones rout ready

--- api/src/routes/index.routes.js
+++ api/src/routes/index.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import * as controller from '../controllers/allControllers.js'
+
 const router = Router();
 
-router.get('/', (req, res) => {
-	
-});
+router.get('/pokemones', controller.getPokemones);
 
 export default router;
