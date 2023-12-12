dd1ee64991b0d505a8dbee13b2e99a3772f28a92
Valentin <valentin.morardo@gmail.com>
2023-12-12 18:26:21 -0300

    fix

--- api/src/controllers/getPokemonesLocal/getPokemonesLocal.controller.js
+++ api/src/controllers/getPokemonesLocal/getPokemonesLocal.controller.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const pokemonesLocalData = require('../../libs/dataLocalPokemones.json')
 export const getPokemonesLocalData = async (req, res) => {
-
+	
 	res.status(200).send(pokemonesLocalData);
 };
