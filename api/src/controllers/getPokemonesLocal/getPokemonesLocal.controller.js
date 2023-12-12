7e062528b719821ea9ddd54c5f049ccc1d05ac54
Valentin <valentin.morardo@gmail.com>
2023-12-12 20:10:24 -0300

    function shuffle for random order to send local pokemones

--- api/src/controllers/getPokemonesLocal/getPokemonesLocal.controller.js
+++ api/src/controllers/getPokemonesLocal/getPokemonesLocal.controller.js
@@ -1,6 +1,19 @@
 import axios from 'axios';
-const pokemonesLocalData = require('../../libs/dataLocalPokemones.json')
+const pokemonesLocalData = require('../../libs/dataLocalPokemones.json');
 export const getPokemonesLocalData = async (req, res) => {
-	
-	res.status(200).send(pokemonesLocalData);
+	function shuffle(pokemones) {
+		for (let i = pokemones.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[pokemones[i], pokemones[j]] = [pokemones[j], pokemones[i]];
+		}
+		return pokemones;
+	}
+
+	const pokemonesOrderRandom = shuffle(pokemonesLocalData);
+
+	res.status(200).send({
+		data: {
+			pokemon_v2_pokemon: pokemonesOrderRandom,
+		},
+	});
 };
